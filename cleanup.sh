b994f70ef1f9b88175b0fc8cbfc18ee7cdbf441c
Marc Paquette <mpaquette@pivotal.io>
2019-09-26 10:39:35 -0400

    updating clean up script a bit. still broken

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,7 @@ source environment/bosh_login.sh
 #delete deployment
 bosh -e $BOSH_ENVIRONMENT  -d cf delete-deployment
 #delete working directories
+#This is dumb to do
 rm -rf $BOSH_LITE_DIRECTORY
 rm -rf $DEPLOYMENT_DIRECTORY
 #delete vms
