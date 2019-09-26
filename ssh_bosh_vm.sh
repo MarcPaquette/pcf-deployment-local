cb5a3683198a1a23e17bb3979b91fa6261dd7716
Marc Paquette <mpaquette@pivotal.io>
2019-09-26 10:47:59 -0400

    moving ssh key to different directory

--- ssh_bosh_vm.sh
+++ ssh_bosh_vm.sh
@@ -1,7 +1,7 @@
 source environment/bosh_env.sh
 source environment/bosh_login.sh
 
-bosh int $DEPLOYMENT_DIRECTORY/creds.yml --path /jumpbox_ssh/private_key > jumpbox.key
-chmod 600 jumpbox.key
-ssh jumpbox@192.168.50.6 -i jumpbox.key
+bosh int $DEPLOYMENT_DIRECTORY/creds.yml --path /jumpbox_ssh/private_key > $DEPLOYMENT_DIRECTORY/jumpbox.key
+chmod 600 $DEPLOYMENT_DIRECTORY/jumpbox.key
+ssh jumpbox@192.168.50.6 -i $DEPLOYMENT_DIRECTORY/jumpbox.key
 
