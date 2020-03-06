030cd967f41d0387badbadc7807295e32b9804f0
Marc Paquette <mpaquette@pivotal.io>
2020-03-06 10:44:34 -0500

    updating errand deployment

--- errand-deployment.sh
+++ errand-deployment.sh
@@ -14,6 +14,22 @@ bosh -e $BOSH_ENVIRONMENT  -d deploy-autoscaling deploy $PCF_DEPLOYMENT_DIRECTOR
   --fix \
   -n
 
+bosh -e $BOSH_ENVIRONMENT  -d push-apps-manager deploy $PCF_DEPLOYMENT_DIRECTORY/errands/push-apps-manager.yml \
+  --vars-file operations/ssh_proxy_backend_tls.yml \
+  --vars-store $DEPLOYMENT_DIRECTORY/deployment-vars.yml \
+  -v system_domain=bosh-lite.com \
+  --recreate \
+  --fix \
+  -n
+
+bosh -e $BOSH_ENVIRONMENT  -d push-usage-service deploy $PCF_DEPLOYMENT_DIRECTORY/errands/push-usage-service.yml \
+  --vars-file operations/ssh_proxy_backend_tls.yml \
+  --vars-store $DEPLOYMENT_DIRECTORY/deployment-vars.yml \
+  -v system_domain=bosh-lite.com \
+  --recreate \
+  --fix \
+  -n
+
 #TODO: This doesn't work
 bosh -e $BOSH_ENVIRONMENT  -d deploy-notifications deploy $PCF_DEPLOYMENT_DIRECTORY/errands/deploy-notifications.yml \
   --vars-file operations/ssh_proxy_backend_tls.yml \
@@ -25,22 +41,6 @@ bosh -e $BOSH_ENVIRONMENT  -d deploy-notifications deploy $PCF_DEPLOYMENT_DIRECT
 
 #TODO: This doesn't work
 bosh -e $BOSH_ENVIRONMENT  -d deploy-notifications-ui deploy $PCF_DEPLOYMENT_DIRECTORY/errands/deploy-notifications-ui.yml \
-  --vars-file operations/ssh_proxy_backend_tls.yml \
-  --vars-store $DEPLOYMENT_DIRECTORY/deployment-vars.yml \
-  -v system_domain=bosh-lite.com \
-  --recreate \
-  --fix \
-  -n
-
-bosh -e $BOSH_ENVIRONMENT  -d push-apps-manager deploy $PCF_DEPLOYMENT_DIRECTORY/errands/push-apps-manager.yml \
-  --vars-file operations/ssh_proxy_backend_tls.yml \
-  --vars-store $DEPLOYMENT_DIRECTORY/deployment-vars.yml \
-  -v system_domain=bosh-lite.com \
-  --recreate \
-  --fix \
-  -n
-
-bosh -e $BOSH_ENVIRONMENT  -d push-usage-service deploy $PCF_DEPLOYMENT_DIRECTORY/errands/push-usage-service.yml \
   --vars-file operations/ssh_proxy_backend_tls.yml \
   --vars-store $DEPLOYMENT_DIRECTORY/deployment-vars.yml \
   -v system_domain=bosh-lite.com \
